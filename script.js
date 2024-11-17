96e11a09b862c3687ce48010fbb8a296aba86530
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-17 14:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
   // your code here
 	
-	let str=s.trim();
+	let str=s.trim().split();
 	if(s.length <=0)return '';
 	return str[0];
 }
-
+ 
 // Do not change the code below
 
 const s = prompt("Enter String:");
